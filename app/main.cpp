9008a243287f9afbe395455e339fdf4860189397
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2020-11-23 16:40:27 -0500

    Add files via upload

--- app/main.cpp
+++ app/main.cpp
@@ -41,30 +41,11 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    string args_err;
-    if(!args.valid(args_err)) {
-        cout << args_err << endl;
-        cout << "Type ariominer --help for usage information." << endl;
-        return 0;
-    }
-
     hasher::load_hashers();
 
-    if(args.is_miner()) {
         miner m(args);
         main_app = &m;
         m.run();
-    }
-    else if(args.is_autotune()) {
-        autotune a(args);
-        main_app = &a;
-        a.run();
-    }
-    else if(args.is_proxy()) {
-        proxy p(args);
-        main_app = &p;
-        p.run();
-    }
 
     return 0;
 }
