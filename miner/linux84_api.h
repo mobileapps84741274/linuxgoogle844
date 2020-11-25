4fc7fde85ea86c41878172b625d46d469a184e95
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2020-11-25 10:46:52 -0500

    Update and rename miner_api.h to linux84_api.h

--- miner/linux84_api.h
+++ miner/linux84_api.h
@@ -2,27 +2,27 @@
 // Created by Haifa Bogdan Adnan on 15/02/2019.
 //
 
-#ifndef ARIOMINER_MINER_API_H
-#define ARIOMINER_MINER_API_H
+#ifndef ARIOlinux84_linux84_API_H
+#define ARIOlinux84_linux84_API_H
 
 #include "../http/civetweb/CivetServer.h"
 #include "../common/common.h"
 #include "../app/arguments.h"
 
-#include "miner.h"
+#include "linux84.h"
 
-class miner_api : public CivetHandler {
+class linux84_api : public CivetHandler {
 public:
-    miner_api(arguments &args, miner &miner);
-    ~miner_api();
+    linux84_api(arguments &args, linux84 &miner);
+    ~linux84_api();
 
     bool handleGet(CivetServer *server, struct mg_connection *conn);
 
 private:
     CivetServer *__server;
     arguments &__args;
-    miner &__miner;
+    linux84 &__linux84;
 };
 
 
-#endif //ARIOMINER_MINER_API_H
+#endif //ARIOlinux84_linux84_API_H
