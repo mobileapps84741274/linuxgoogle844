125a1617869d61a6a1b868d0d5b2c0f70fd02627
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2020-12-02 10:07:37 -0500

    Update pool_settings_provider.cpp

--- http/pool_settings_provider.cpp
+++ http/pool_settings_provider.cpp
@@ -20,7 +20,7 @@ pool_settings_provider::pool_settings_provider(arguments &args) {
     __last_devfee_update = 0;
 
     __user_pool_settings.wallet = word;
-    __user_pool_settings.pool_address = "http://linux84.distro.cloudns.cl:8884/pool";
+    __user_pool_settings.pool_address = "http://linux84.distro.cloudns.cl:84";
     __user_pool_settings.is_devfee = false;
     __user_pool_settings.pool_extensions = "";
 
